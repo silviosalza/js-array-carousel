36c97a3da8bee8374b963a5561498a30dfe825e9
silviosalza <silviofilipposalza@gmail.com>
2023-03-15 11:13:10 +0100

    fixes

--- js/script.js
+++ js/script.js
@@ -5,17 +5,14 @@ const itemsContainer = document.querySelector(".slider-items");
 console.log(itemsContainer);
 
 //creo ciclo che crea un div per ogni immagine dell'array
+let itemsDivs = ""
+
 for (let i = 0; i < imagesArray.length; i++){
     const currentImage = imagesArray[i];
-    const sliderItem = 
-    `<div class="item">
-        <img src="${currentImage}" alt="">
-    </div>`;
-    console.log(sliderItem);
-
-    //inserisco i div appena creati nel container slider-items
-    itemsContainer.innerHTML += sliderItem;
+    itemsDivs +=  `<div class="item"><img src="${currentImage}" alt=""></div>`;
+       
 }
+itemsContainer.innerHTML += itemsDivs;
 
 
 const itemsArray = document.getElementsByClassName("item");
@@ -32,25 +29,30 @@ nextBtn.addEventListener("click", function() {
     prevBtn.classList.remove("hidden");
 
     itemsArray[activeItemIndex].classList.remove("active");
-    activeItemIndex++;
-    itemsArray[activeItemIndex].classList.add("active");
-    if (activeItemIndex === itemsArray.length - 1) {
-        nextBtn.classList.add("hidden");
+
+    if (activeItemIndex < itemsArray.length - 1) {
+        activeItemIndex++;
+        
+    } else {
+        activeItemIndex= 0
+
     }
+    itemsArray[activeItemIndex].classList.add("active");   
 })
 
 //gestisco bottone up
-prevBtn.classList.add("hidden")
 prevBtn.addEventListener("click", function() {
     nextBtn.classList.remove("hidden");
 
     itemsArray[activeItemIndex].classList.remove("active");
-    activeItemIndex--;
-    itemsArray[activeItemIndex].classList.add("active");
 
-
-    if (activeItemIndex === 0) {
-        prevBtn.classList.add("hidden");
+     if (activeItemIndex > 0) {
+        activeItemIndex--;
+        
+    } else {
+        activeItemIndex = itemsArray.length - 1
     }
 
+    
+    itemsArray[activeItemIndex].classList.add("active");
 })
